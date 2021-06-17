399a757e9928b4841b0f4da15da2c3234daaed0a
Nauras Jabari <nauras@agencyundone.com>
2021-06-17 15:22:42 -0400

    chore: testing

--- modules/nuxt-module-ipfs/plugin.js
+++ modules/nuxt-module-ipfs/plugin.js
@@ -9,31 +9,20 @@ console.log(`🔌 [Module | NuxtModuleIpfs] Methods`);
 // /////////////////////////////////////////////////////////////////// Functions
 // -----------------------------------------------------------------------------
 // ------------------------------------------------------------------ relativity
-// const Relativity = (path) => {
-//   if (process.env.NODE_ENV !== 'development') {
-//     const append = path.charAt(0) === '/' ? path.slice(1) : path;
-//     if (typeof window !== 'undefined') {
-//       const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
-//       const ipfsPathPrefix =
-//         (window.location.pathname.match(ipfsPathRegExp) || [])[1] || '';
-//       console.log(ipfsPathPrefix);
-//       if (ipfsPathPrefix) {
-//         console.log(`${ipfsPathPrefix}${path}`);
-//         return `${ipfsPathPrefix}${path}`;
-//       } else {
-//         return path;
-//       }
-//     }
-//     return `/relativity/${append}`;
-//   }
-//   return path;
-// };
-
 const Relativity = (path) => {
-  const append = path.charAt(0) === '/' ? path.slice(1) : path;
   if (process.env.NODE_ENV !== 'development') {
+    const append = path.charAt(0) === '/' ? path.slice(1) : path;
     if (typeof window !== 'undefined') {
-      return path;
+      const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
+      const ipfsPathPrefix =
+        (window.location.pathname.match(ipfsPathRegExp) || [])[1] || '';
+      console.log(ipfsPathPrefix);
+      if (ipfsPathPrefix) {
+        console.log(`${ipfsPathPrefix}${path}`);
+        return `${ipfsPathPrefix}${path}`;
+      } else {
+        return path;
+      }
     }
     return `/relativity/${append}`;
   }
