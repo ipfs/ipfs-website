f45ffbec2b90b85ce6957a972107623e56ebdf89
Nauras Jabari <nauras@agencyundone.com>
2021-06-17 13:57:57 -0400

    feat: added client-side ipfs/ipns url prefix checking and appending

--- modules/nuxt-module-ipfs/plugin.js
+++ modules/nuxt-module-ipfs/plugin.js
@@ -10,10 +10,17 @@ console.log(`🔌 [Module | NuxtModuleIpfs] Methods`);
 // -----------------------------------------------------------------------------
 // ------------------------------------------------------------------ relativity
 const Relativity = (path) => {
-  const append = path.charAt(0) === '/' ? path.slice(1) : path;
   if (process.env.NODE_ENV !== 'development') {
+    const append = path.charAt(0) === '/' ? path.slice(1) : path;
     if (typeof window !== 'undefined') {
-      return path;
+      const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
+      const ipfsPathPrefix =
+        (window.location.pathname.match(ipfsPathRegExp) || [])[1] || '';
+      console.log(ipfsPathPrefix);
+      if (ipfsPathPrefix) {
+        console.log(`${ipfsPathPrefix}${path}`);
+        return `${ipfsPathPrefix}${path}`;
+      }
     }
     return `/relativity/${append}`;
   }
