87f198af62a2e258e0a98d47531788a55520500d
Jessica Schilling <jessica@protocol.ai>
2021-06-23 12:02:11 -0600

    Update pages/index.vue
    Co-authored-by: MollyM <momack2@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -546,10 +546,8 @@
             <p class="leading-normal">
               If you add a new version of your file to IPFS, its cryptographic
               hash is different, and so it gets a new CID. This means
-              <strong
-                >files stored on IPFS are resistant to tampering and
-                censorship</strong
-              >
+              <strong>files stored on IPFS are resistant to tampering and
+                censorship</strong>
               — any changes to a file don't overwrite the original, even though
               common chunks of bigger files are reused in order to minimize
               storage costs.
