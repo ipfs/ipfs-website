f3688bd1c31622b95764c34057d1a2da89b587c9
Nauras Jabari <nauras@agencyundone.com>
2021-06-24 09:22:11 -0400

    chore: testing

--- modules/nuxt-module-ipfs/plugin.js
+++ modules/nuxt-module-ipfs/plugin.js
@@ -11,11 +11,19 @@ console.log(`🔌 [Module | NuxtModuleIpfs] Methods`)
 // ------------------------------------------------------------------ relativity
 const Relativity = function (path) {
   if (process.client) {
-    let ipfsMatch = window.location.pathname.match(/\/ipfs\/[^/]+\//)
-    console.log(path)
-    console.log(ipfsMatch)
-    console.log(ipfsMatch ? ipfsMatch[0] + path : path)
-    return ipfsMatch ? ipfsMatch[0] + path : path
+    // let ipfsMatch = window.location.pathname.match(/\/ipfs\/[^/]+\//)
+    // console.log(path)
+    // console.log(ipfsMatch)
+    // console.log(ipfsMatch ? ipfsMatch[0] + path : path)
+    // return ipfsMatch ? ipfsMatch[0] + path : path
+    const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/
+    console.log(ipfsPathRegExp)
+    const ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || ''
+    console.log(ipfsPathPrefix)
+    const base = ipfsPathPrefix ? `${ipfsPathPrefix}/` : ''
+    console.log(base)
+    console.log(path.charAt(0) === '/' ? `${base}${path.slice(1)}` : path)
+    return path.charAt(0) === '/' ? `${base}${path.slice(1)}` : path
   }
   return ''
 }
