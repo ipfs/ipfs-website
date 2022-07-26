5d77c4a7e72d4fe571dcc707ef3088dd29006774
Marcin Rataj <lidel@lidel.org>
2022-07-27 01:56:23 +0200

    refactor: smart redirect ipfs.io → ipfs.tech
    Switching to location.replace() because is ensures replaced .io URLs
    will not be saved in browser history

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,8 @@ export default {
 
       // redirect 'ipfs.io' to the new canonical domain name 'ipfs.tech'
       // https://github.com/protocol/bifrost-infra/issues/178#issuecomment-1195867284
-      const { hostname, pathname } = window.location;
+      const { hostname, pathname, href: url } = window.location;
+
       // regular ipfs.io
       if (
         hostname === 'ipfs.io' &&
@@ -23,22 +24,29 @@ export default {
           pathname.startsWith('/ipns/ipfs.io/') || pathname.startsWith('/ipfs/')
         )
       ) {
-        window.location.hostname = hostname.replace('ipfs.io', 'ipfs.tech');
+        window.location.replace(url.replace('//ipfs.io/', '//ipfs.tech/'));
       }
       // subdomain gateways (no tls)
       if (hostname.startsWith('ipfs.io.ipns.')) {
-        window.location.hostname = hostname.replace('ipfs.io', 'ipfs.tech');
+        window.location.replace(
+          url.replace('//ipfs.io.ipns.', '//ipfs.tech.ipns.')
+        );
       }
       // subdomain gateways (inlined)
       if (hostname.startsWith('ipfs-io.ipns.')) {
-        window.location.hostname = hostname.replace('ipfs-io', 'ipfs-tech');
+        window.location.replace(
+          url.replace('//ipfs-io.ipns.', '//ipfs-tech.ipns.')
+        );
       }
       // path gateways
       if (pathname.startsWith('/ipns/ipfs.io/')) {
-        window.location.pathname = pathname.replace(
-          '/ipns/ipfs.io/',
-          '/ipns/ipfs.tech/'
+        window.location.replace(
+          url.replace('/ipns/ipfs.io/', '/ipns/ipfs.tech/')
         );
+      }
+      // drop www. if typed in address bar for some reason
+      if (hostname.startsWith('www.ipfs.')) {
+        window.location.replace(url.replace('//www.ipfs.', '//ipfs.'));
       }
     }
   },
