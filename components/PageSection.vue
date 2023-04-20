7dc7da4fcdcbec2abcaad2b7f5b38e2ee252ff2b
Tom Benbow <tom@hellomoon.net>
2023-04-20 12:20:15 -0500

    Remote template literal from section class attribute

--- components/PageSection.vue
+++ components/PageSection.vue
@@ -9,7 +9,7 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-  <section class="`relative`">
+  <section class="relative">
     <div v-if="props.backgroundColor" :class="`absolute -z-1 w-full h-full ${backgroundColor}`" />
     <img v-if="props.background" :src="`./images/${background}`" alt="" class="absolute -z-1 w-full h-full object-cover">
     <img v-if="props.overlay" :src="`./images/${overlay}`" alt="" :class="`absolute ${overlayPosition}`">
