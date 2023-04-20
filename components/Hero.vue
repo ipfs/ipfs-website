f8a97fe28aef56aa79dc32dafe1f208dcec65a37
Tom Benbow <tom@hellomoon.net>
2023-04-20 12:52:26 -0500

    Add a max-width to content area in hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,11 +9,11 @@ const props = defineProps<Props>()
 
 <template>
   <PageSection id="hero" :background="props.background" :overlay="props.overlay" background-color="bg-brand-dark" overlay-position="top-0 right-0">
-    <div class="p-y-52">
-      <h1 class="text-5xl text-center text-white m-b-5 m-x-auto">
+    <div class="p-y-52 max-w-xl m-x-auto">
+      <h1 class="text-5xl text-center text-white m-b-5">
         <slot name="title" />
       </h1>
-      <h4 class="text-lg text-center font-normal text-gray-400 m-x-auto">
+      <h4 class="text-lg text-center font-normal text-gray-400">
         <slot name="subtitle" />
       </h4>
     </div>
