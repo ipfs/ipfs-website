33a4e75f3efa1fbb9b31acc898515e8083b94f09
Tom Benbow <tom@hellomoon.net>
2023-04-21 22:09:12 -0500

    Update newsletter component

--- components/NewsletterForm.vue
+++ components/NewsletterForm.vue
@@ -1,19 +1,23 @@
+<script>
+export default {
+  props: {},
+  data: () => ({
+    email: null,
+  }),
+  methods: {
+    onSubmit() {
+      /* this.$countly.trackEvent(this.$countly.events.NEWSLETTER_SUBSCRIBE); */
+    },
+  },
+}
+</script>
+
 <template>
-  <div
-    id="newsletter-form"
-    class="flex flex-col lg:flex-row lg:justify-between lg:items-center"
-  >
-    <div class="flex-shrink lg:max-w-lg xl:max-w-xl mb-4 lg:mb-0">
-      <h2>Stay informed</h2>
-      <p class="mt-2 mr-2">
-        Sign up for the IPFS Weekly newsletter (<a
-          class="text-blueGreenLight hover:underline"
-          href="https://blog.ipfs.tech/?tags=weekly"
-          >example</a
-        >) for the latest on releases, upcoming developments, community events,
-        and more.
-      </p>
-    </div>
+  <div id="newsletter-form">
+    <h2 class="text-lg mb-4">Stay informed</h2>
+    <p class="text-sm mb-8 ">
+      Sign up for the IPFS Weekly newsletter (<a class="text-blueGreenLight hover:underline" href="https://blog.ipfs.tech/?tags=weekly">example</a>) for the latest on releases, upcoming developments, community events, and more.
+    </p>
     <form
       id="mc-embedded-subscribe-form"
       name="mc-embedded-subscribe-form"
@@ -31,18 +35,18 @@
             required
             type="email"
             aria-label="Email Address"
-            class="flex-grow text-black p-2 rounded"
+            class="flex-grow text-black p-2 pr-8 rounded-full"
             placeholder="email@your.domain"
             name="EMAIL"
-          />
-          <div class="sm:ml-4 sm:pt-0 pt-2">
+          >
+          <div class="-ml-8">
             <input
               id="mc-embedded-subscribe"
               type="submit"
               value="Subscribe"
               name="subscribe"
-              class="p-2 text-white font-semibold bg-blueGreen hover:bg-blueGreenScreen transition duration-300 rounded cursor-pointer w-full"
-            />
+              class="btn btn-primary"
+            >
           </div>
         </div>
         <label class="pt-2 col-start-1 col-span-2" for="gdpr_28879">
@@ -53,45 +57,14 @@
             required
             name="gdpr[28879]"
             value="Y"
-          /><span class="pl-2">Please send me the newsletter</span>
+          ><span class="pl-2">Please send me the newsletter</span>
         </label>
       </div>
       <div id="mergeRow-gdpr">
         <div style="position: absolute; left: -5000px" aria-hidden="true">
-          <input
-            type="text"
-            name="b_25473244c7d18b897f5a1ff6b_cad54b2230"
-            tabindex="-1"
-            value=""
-          />
+          <input type="text" name="b_25473244c7d18b897f5a1ff6b_cad54b2230" tabindex="-1" value="">
         </div>
       </div>
     </form>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'NewsletterForm',
-  props: {},
-  data: () => ({
-    email: null,
-  }),
-  methods: {
-    onSubmit() {
-      this.$countly.trackEvent(this.$countly.events.NEWSLETTER_SUBSCRIBE);
-    },
-  },
-};
-</script>
-
-<style scoped>
-h2 {
-  @apply font-display;
-
-  font-weight: 500;
-  font-size: 1.75rem;
-  line-height: 1.2;
-  letter-spacing: -0.01em;
-}
-</style>
