07dc676d6fdfe10f1e5d06dfc5206b456a833723
Tom Benbow <tom@hellomoon.net>
2023-04-25 07:08:09 -0500

    feat: add community and developers pages (#206)

---
+++ pages/community.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <seo-tags
+      title="IPFS Powers the Distributed Web"
+      description="The InterPlanetary File System is a peer-to-peer hypermedia protocol designed to preserve and grow humanity's knowledge by making the web upgradeable, resilient, and more open."
+    />
+    <Hero background="community-hero.jpg">
+      <template #title>
+        Community: Get in Orbit
+      </template>
+      <template #subtitle>
+        IPFS is made of you – our open source ecosystem of nearly 2,000 active contributors and many more developers and users.
+      </template>
+    </Hero>
+  </div>
+</template>
