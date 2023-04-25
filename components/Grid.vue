b1f6c7ed91d640a45b8736ebd066c08d68673400
Tom Benbow <tom@hellomoon.net>
2023-04-25 14:21:25 -0500

    Medium grid columns

--- components/Grid.vue
+++ components/Grid.vue
@@ -8,7 +8,7 @@ defineProps<Props>()
 </script>
 
 <template>
-  <div class="grid gap-8 grid-cols-1" :class="`lg:grid-cols-${cols || 3}`">
+  <div class="grid gap-8 md:grid-cols-2 grid-cols-1" :class="`lg:grid-cols-${cols || 3}`">
     <slot />
   </div>
 </template>
