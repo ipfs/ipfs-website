71908567e4ca27c62662b20636b9f0bd25b15c46
Chris Waring <106938+cwaring@users.noreply.github.com>
2023-04-26 18:19:09 +0100

    fix: temp remove broken comps

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,64 +1,61 @@
-<template>
-  <div class="flex flex-col min-h-screen font-sans">
-    <Header />
-    <MobileMenu />
-    <slot />
-    <Footer />
-  </div>
-</template>
-
 <script>
 export default {
   mounted() {
     if (process.client && window) {
-      window.history.scrollRestoration = 'auto';
+      window.history.scrollRestoration = 'auto'
 
       // redirect 'ipfs.io' to the new canonical domain name 'ipfs.tech'
       // https://github.com/protocol/bifrost-infra/issues/178#issuecomment-1195867284
-      const { hostname, pathname, href: url } = window.location;
+      const { hostname, pathname, href: url } = window.location
 
       // regular ipfs.io
       if (
-        hostname === 'ipfs.io' &&
-        !(
+        hostname === 'ipfs.io'
+        && !(
           pathname.startsWith('/ipns/') || pathname.startsWith('/ipfs/')
         )
-      ) {
-        window.location.replace(url.replace('//ipfs.io/', '//ipfs.tech/'));
-      }
+      )
+        window.location.replace(url.replace('//ipfs.io/', '//ipfs.tech/'))
+
       // subdomain gateways (no tls)
       if (hostname.startsWith('ipfs.io.ipns.')) {
         window.location.replace(
-          url.replace('//ipfs.io.ipns.', '//ipfs.tech.ipns.')
-        );
+          url.replace('//ipfs.io.ipns.', '//ipfs.tech.ipns.'),
+        )
       }
       // subdomain gateways (inlined)
       if (hostname.startsWith('ipfs-io.ipns.')) {
         window.location.replace(
-          url.replace('//ipfs-io.ipns.', '//ipfs-tech.ipns.')
-        );
+          url.replace('//ipfs-io.ipns.', '//ipfs-tech.ipns.'),
+        )
       }
       // path gateways
       if (pathname.startsWith('/ipns/ipfs.io/')) {
         window.location.replace(
-          url.replace('/ipns/ipfs.io/', '/ipns/ipfs.tech/')
-        );
+          url.replace('/ipns/ipfs.io/', '/ipns/ipfs.tech/'),
+        )
       }
       // drop www. if typed in address bar for some reason
-      if (hostname.startsWith('www.ipfs.')) {
-        window.location.replace(url.replace('//www.ipfs.', '//ipfs.'));
-      }
+      if (hostname.startsWith('www.ipfs.'))
+        window.location.replace(url.replace('//www.ipfs.', '//ipfs.'))
+
       // ipfs.network → ipfs.tech (we used it before we got .tech)
-      if (url.includes('ipfs.network')) {
-        window.location.replace(url.replace('ipfs.network', 'ipfs.tech'));
-      }
-      if (url.includes('ipfs-network')) {
-        window.location.replace(url.replace('ipfs-network', 'ipfs-tech'));
-      }
+      if (url.includes('ipfs.network'))
+        window.location.replace(url.replace('ipfs.network', 'ipfs.tech'))
+
+      if (url.includes('ipfs-network'))
+        window.location.replace(url.replace('ipfs-network', 'ipfs-tech'))
     }
   },
-};
+}
 </script>
+
+<template>
+  <div class="flex flex-col min-h-screen font-sans">
+    <slot />
+    <Footer />
+  </div>
+</template>
 
 <style lang="postcss">
 /* banner styles */
--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <Header no-hero />
-    <MobileMenu />
     <Nuxt />
     <Footer />
   </div>
