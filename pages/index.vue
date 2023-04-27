a2149de5f41210b9f1fd1c3157d2a593584a718d
Tom Benbow <tom@hellomoon.net>
2023-04-27 17:51:24 -0500

    Add ribbon component

--- pages/index.vue
+++ pages/index.vue
@@ -176,6 +176,32 @@
         <p class="text-xl mb-8">
           Join thousands of developers who choose IPFS to build software that’s open, verifiable, and resilient.
         </p>
+        <div class="ribbon-wrap py-56">
+          <RibbonAnnotations
+            :annotations="[
+              'Application-Developers',
+              'Scientists',
+              'Infrastructure Developers',
+              'Researchers',
+              'Designers',
+              'Archivists',
+              'Yes, Even the occasional Astronaut',
+              'Artists',
+            ]"
+          />
+          <Ribbon
+            :images="[
+              'ribbon-home-1.jpg',
+              'ribbon-home-2.jpg',
+              'ribbon-home-3.jpg',
+              'ribbon-home-4.jpg',
+              'ribbon-home-5.jpg',
+              'ribbon-home-6.jpg',
+              'ribbon-home-7.jpg',
+              'ribbon-home-8.jpg',
+            ]"
+          />
+        </div>
         <p class="text-xl mb-8">
           <b>Our protocol's success hinges on participation from all of our neighbors:</b> Every new node that joins IPFS makes our network stronger for everyone else.
         </p>
