fc1f8d452a74d54a103f7baa971d7f574ded1e5f
Chris Waring <106938+cwaring@users.noreply.github.com>
2023-05-06 21:55:46 +0100

    refactor twitter comps

--- components/TwitterCard.vue
+++ components/TwitterCard.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
 interface Props {
-  card: {
-    name?: string
-    handle?: string
-    post?: string
-    image?: string
-    time?: string
-    date?: string
-    retweet?: {
-      name?: string
-      handle?: string
-      post?: string
-      image?: string
-      time?: string
-      date?: string
-    }
-  }
+  name?: string
+  handle?: string
+  post?: string
+  image?: string
+  time?: string
+  date?: string
 }
 defineProps<Props>()
 </script>
@@ -23,33 +13,22 @@ defineProps<Props>()
 <template>
   <div class="twitter-card mb-8 break-inside-avoid rounded-xl bg-brand-light p-4">
     <div class="mb-4 w-full flex items-center gap-2">
-      <img :src="`/images/twitter-profile-${card.handle}.jpg`" :alt="card.handle" class="h-9 w-9 flex-none rounded-full">
+      <img :src="`/images/twitter-profile-${handle}.jpg`" :alt="handle" class="h-9 w-9 flex-none rounded-full">
       <div class="flex-1">
         <p class="font-bold">
-          {{ card.name }}
+          {{ name }}
         </p>
-        <p>@{{ card.handle }}</p>
+        <p>@{{ handle }}</p>
       </div>
       <img src="/images/twitter-bird.svg" alt="twitter-bird" class="h-7 w-8 flex-none">
     </div>
     <div class="twitter-post mb-4">
-      <MarkdownContent v-if="card.post" :content="card.post" />
+      <slot />
     </div>
-    <img v-if="card.image" :src="`/images/${card.image}`" class="mb-4 w-full">
-    <div v-if="card.retweet" class="mb-4 border rounded-md p-3">
-      <div class="mb-2 w-full flex items-center gap-1">
-        <img :src="`/images/twitter-profile-${card.handle}.jpg`" :alt="card.handle" class="h-5 w-5 flex-none rounded-full">
-        <p class="flex-1">
-          <span class="font-bold">{{ card.retweet.name }}</span> @{{ card.retweet.handle }}, {{ card.retweet.date }}
-        </p>
-      </div>
-      <div class="twitter-post">
-        <MarkdownContent v-if="card.retweet.post" :content="card.retweet.post" />
-      </div>
-      <img v-if="card.retweet.image" :src="`/images/${card.retweet.image}`" class="mt-4 w-full">
-    </div>
+    <img v-if="image" :src="`/images/${image}`" class="mb-4 w-full">
+    <slot name="retweet" />
     <p>
-      {{ card.time }} • {{ card.date }}
+      {{ time }} • {{ date }}
     </p>
   </div>
 </template>
---
+++ components/TwitterRetweet.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+interface RetweetProps {
+  name?: string
+  handle?: string
+  post?: string
+  image?: string
+  time?: string
+  date?: string
+}
+
+defineProps<RetweetProps>()
+</script>
+
+<template>
+  <div class="mb-4 border rounded-md p-3">
+    <div class="mb-2 w-full flex items-center gap-1">
+      <img :src="`/images/twitter-profile-${handle}.jpg`" :alt="handle" class="h-5 w-5 flex-none rounded-full">
+      <p class="flex-1">
+        <span class="font-bold">{{ name }}</span> @{{ handle }}, {{ date }}
+      </p>
+    </div>
+    <div class="twitter-post">
+      <slot />
+    </div>
+    <img v-if="image" :src="`/images/${image}`" class="mt-4 w-full">
+  </div>
+</template>
