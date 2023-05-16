c85254229e7af9d1655a7b8dcffcf83e4602b8f7
Chris Waring <106938+cwaring@users.noreply.github.com>
2023-05-16 15:49:19 +0100

    add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-const CDOMAIN = 'ipfs.tech'
+const PUBLIC_SITE_URL = process.env.NUXT_PUBLIC_SITE_URL || 'https://ipfs.tech'
+const PUBLIC_DOMAIN = PUBLIC_SITE_URL.replace(/(^\w+:|^)\/\//, '').replace(/\/$/, '')
 
 export default defineNuxtConfig({
   nitro: {
@@ -30,14 +31,23 @@ export default defineNuxtConfig({
     '@unocss/nuxt',
     '@nuxtjs/plausible',
   ],
+  extends: [
+    'nuxt-seo-kit',
+  ],
   experimental: {
     inlineSSRStyles: false,
     componentIslands: true,
   },
   runtimeConfig: {
     public: {
+      siteUrl: PUBLIC_SITE_URL,
+      siteName: 'IPFS',
+      siteDescription: 'The InterPlanetary File System is a peer-to-peer hypermedia protocol designed to preserve and grow humanity\'s knowledge by making the web upgradeable, resilient, and more open.',
+      language: 'en',
+      indexable: false, // disable indexing for until launch
       plausible: {
-        domain: CDOMAIN,
+        // strip out the protocol and trailing slash
+        domain: PUBLIC_DOMAIN,
       },
     },
   },
