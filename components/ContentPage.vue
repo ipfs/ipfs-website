ddd98858b12f00af2f994ee3c200b86a37efef84
Chris Waring <106938+cwaring@users.noreply.github.com>
2023-06-07 14:59:58 +0100

    add ContentPage wrapper comp

---
+++ components/ContentPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <ContentDoc>
+      <template #default="{ doc }">
+        <Hero background="community-hero.jpg">
+          <template #title>
+            {{ doc.title }}
+          </template>
+          <template #subtitle>
+            {{ doc.description }}
+          </template>
+        </Hero>
+        <PageSection>
+          <div class="text-base prose prose-truegray xl:text-xl">
+            <ContentRenderer :value="doc" />
+          </div>
+        </PageSection>
+      </template>
+      <template #not-found>
+        <h1>Oops, this page is missing</h1>
+      </template>
+    </ContentDoc>
+  </main>
+</template>
