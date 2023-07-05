d9a41937cf9889dc7e2d6caff2e9dbe773206c8f
Chris Waring <106938+cwaring@users.noreply.github.com>
2023-07-05 19:35:38 +0100

    rm dev mode

--- app.vue
+++ app.vue
@@ -1,13 +1,4 @@
 <script setup lang="ts">
-const isDev = import.meta.env.DEV
-
-useHead({
-  htmlAttrs: {
-    class: {
-      'mode-dev': isDev,
-    },
-  },
-})
 useSeoMeta({
   twitterSite: '@ipfs',
 })
@@ -23,14 +14,5 @@ useSeoMeta({
 <style>
 html {
   scroll-behavior: smooth;
-}
-
-/* debug mode */
-.mode-dev *:hover {
-  outline: 1px solid red
-}
-
-.mode-dev .setup {
-  display: block;
 }
 </style>
