6f5f053f85fbf7945fcf2ee02388df0dbd34f63b
cw <106938+cwaring@users.noreply.github.com>
2023-08-03 15:23:27 +0100

    chore: update deps (#269)
    * chore: update deps
    
    * refactor: default layout fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,46 @@
-<script>
-export default {
-  mounted() {
-    if (process.client && window) {
-      window.history.scrollRestoration = 'auto'
+<script setup lang="ts">
+onMounted(() => {
+  // !window safety
+  if (typeof window === 'undefined')
+    return
 
-      // redirect 'ipfs.io' to the new canonical domain name 'ipfs.tech'
-      // https://github.com/protocol/bifrost-infra/issues/178#issuecomment-1195867284
-      const { hostname, pathname, href: url } = window.location
+  // redirect 'ipfs.io' to the new canonical domain name 'ipfs.tech'
+  // https://github.com/protocol/bifrost-infra/issues/178#issuecomment-1195867284
+  const { hostname, pathname, href: url } = window.location
 
-      // regular ipfs.io
-      if (
-        hostname === 'ipfs.io'
-        && !(
-          pathname.startsWith('/ipns/') || pathname.startsWith('/ipfs/')
-        )
-      )
-        window.location.replace(url.replace('//ipfs.io/', '//ipfs.tech/'))
+  // regular ipfs.io
+  if (hostname === 'ipfs.io' && !(pathname.startsWith('/ipns/') || pathname.startsWith('/ipfs/')))
+    window.location.replace(url.replace('//ipfs.io/', '//ipfs.tech/'))
 
-      // subdomain gateways (no tls)
-      if (hostname.startsWith('ipfs.io.ipns.')) {
-        window.location.replace(
-          url.replace('//ipfs.io.ipns.', '//ipfs.tech.ipns.'),
-        )
-      }
-      // subdomain gateways (inlined)
-      if (hostname.startsWith('ipfs-io.ipns.')) {
-        window.location.replace(
-          url.replace('//ipfs-io.ipns.', '//ipfs-tech.ipns.'),
-        )
-      }
-      // path gateways
-      if (pathname.startsWith('/ipns/ipfs.io/')) {
-        window.location.replace(
-          url.replace('/ipns/ipfs.io/', '/ipns/ipfs.tech/'),
-        )
-      }
-      // drop www. if typed in address bar for some reason
-      if (hostname.startsWith('www.ipfs.'))
-        window.location.replace(url.replace('//www.ipfs.', '//ipfs.'))
+  // subdomain gateways (no tls)
+  if (hostname.startsWith('ipfs.io.ipns.')) {
+    window.location.replace(
+      url.replace('//ipfs.io.ipns.', '//ipfs.tech.ipns.'),
+    )
+  }
+  // subdomain gateways (inlined)
+  if (hostname.startsWith('ipfs-io.ipns.')) {
+    window.location.replace(
+      url.replace('//ipfs-io.ipns.', '//ipfs-tech.ipns.'),
+    )
+  }
+  // path gateways
+  if (pathname.startsWith('/ipns/ipfs.io/')) {
+    window.location.replace(
+      url.replace('/ipns/ipfs.io/', '/ipns/ipfs.tech/'),
+    )
+  }
+  // drop www. if typed in address bar for some reason
+  if (hostname.startsWith('www.ipfs.'))
+    window.location.replace(url.replace('//www.ipfs.', '//ipfs.'))
 
-      // ipfs.network → ipfs.tech (we used it before we got .tech)
-      if (url.includes('ipfs.network'))
-        window.location.replace(url.replace('ipfs.network', 'ipfs.tech'))
+  // ipfs.network → ipfs.tech (we used it before we got .tech)
+  if (url.includes('ipfs.network'))
+    window.location.replace(url.replace('ipfs.network', 'ipfs.tech'))
 
-      if (url.includes('ipfs-network'))
-        window.location.replace(url.replace('ipfs-network', 'ipfs-tech'))
-    }
-  },
-}
+  if (url.includes('ipfs-network'))
+    window.location.replace(url.replace('ipfs-network', 'ipfs-tech'))
+})
 </script>
 
 <template>
